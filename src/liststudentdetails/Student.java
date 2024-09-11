e5245e77a1e42c3ada14a26ed1bc8ee5757b3e21
Sahnavaz-Choksi <chokssah@sheridancollege.ca>
2024-09-11 18:07:21 -0400

    Student list version 1

---
+++ src/liststudentdetails/Student.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package liststudentdetails;
+
+/**
+ *
+ * @author SAHNAVAZ CHOKSI
+ */
+public class Student {
+    private String name;
+    private int sid;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+}
